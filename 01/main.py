a23e8b0fc848c2cb273ba328f75ffa459fad3b27
appollo30 <leo.lasnier@me.com>
2024-12-01 13:07:24 +0100

    part 2 of day 1 also done, it works babyyy

--- 01/main.py
+++ 01/main.py
@@ -3,13 +3,15 @@ Day 01 of the Advent of Code 2024
 https://adventofcode.com/2024/day/1
 """
 from src.parser import parse
-from src.solution import main
+from src.solution import part_1, part_2
 
 if __name__ == "__main__":
     FILE_NAME = "input.txt"
     t = parse(FILE_NAME)
     if t is not None:
         list1, list2 = t
-        result = main(list1,list2)
-        print(f"The added distances are : {result}")
+        result_part_1 = part_1(list1,list2)
+        print(f"The added distances are : {result_part_1}")
+        result_part_2 = part_2(list1,list2)
+        print(f"The similarity between both lists is : {result_part_2}")
     
--- 01/src/solution.py
+++ 01/src/solution.py
@@ -3,16 +3,22 @@ Solution for day 01 of advent of code 2024
 https://adventofcode.com/2024/day/1
 """
 from typing import List
+from collections import Counter
 
-def main(list1 : List[int],list2 : List[int]) -> int:
+def part_1(list1 : List[int],list2 : List[int]) -> int:
     """
-    Solution for day 1,
+    Solution for part 1 of day 1,
     First we sort both lists in ascending order (or descending order can also work), 
-    then we do a termwise absolute difference, and finally we sum all of those together
+    then we do a termwise absolute difference, and finally we sum all of those together.
+    
+    Worst-case time complexity : O(n*log(n)) 
+    
+    Explanation : The sorting of both lists is O(n*log(n)) 
+    (Timsort https://en.wikipedia.org/wiki/Timsort), and the termwise sum is O(n)
 
     Args:
-        list1 (List): the first list
-        list2 (List): the second list
+        list1 (List[int]): the first list
+        list2 (List[int]): the second list
 
     Returns:
         int: the result of the termwise distances
@@ -20,7 +26,38 @@ def main(list1 : List[int],list2 : List[int]) -> int:
     n = len(list1)
     list1.sort()
     list2.sort()
-    count = 0
+    result = 0
     for i in range(n):
-        count += abs(list1[i]-list2[i])
-    return count
+        result += abs(list1[i]-list2[i])
+    return result
+
+def part_2(list1 : List[int], list2 : List[int]) -> int:
+    """
+    Solution for part 2 of day 1,
+    First we use a Counter to count the occurences of every element in list2,
+    then we loop over list1 to check how many occurences of the elements of list1 in list2.
+    And we finally have our result that is the product between each element of list1,
+    and the number of times it appears in list2.
+    
+    Worst-case complexity : O(n)
+    
+    Explanation : The instanciation of the Counter takes O(n)
+    (The size of list2)
+    Then we loop over n, so still O(n).
+
+    Args:
+        list1 (List[int]): the first list
+        list2 (List[int]): the second list
+
+    Returns:
+        int: the similarity between both lists
+    """
+    n = len(list1)
+    c = Counter(list2)
+    result = 0
+    for i in range(n):
+        element = list1[i]
+        # Python counters do not raise errors if an element is not present, so we can directly
+        # access it without checking if element is in c
+        result += element*c[element]
+    return result
