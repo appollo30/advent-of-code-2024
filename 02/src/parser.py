e594dd904397d1fc276568fe69b00ad45c167e92
appollo30 <leo.lasnier@me.com>
2024-12-02 20:20:02 +0100

    did some linting, part 1 of day 2 is complete

--- 02/src/parser.py
+++ 02/src/parser.py
@@ -2,8 +2,18 @@
 Module to parse the input file, and output a list of integer lists.
 """
 import os
+from typing import List, Optional
 
-def parse(file_name):
+def parse(file_name : Optional[str]) -> Optional[List[List[int]]]:
+    """
+    Parses the input file
+
+    Args:
+        file_name (Optional[str]): The name of the input file
+
+    Returns:
+        Optional[List[List[int]]]: None, or the list of reports
+    """
     file_path = os.path.join(os.getcwd(),"data",file_name)
     if not os.path.exists(file_path):
         print(f"File \"{file_name}\" does not exist")
--- 02/src/solution.py
+++ 02/src/solution.py
@@ -1,4 +1,31 @@
-def part_1(reports):
+"""
+Solution for day 02 of advent of code 2024
+https://adventofcode.com/2024/day/2
+"""
+from typing import List
+
+def part_1(reports : List[List[int]]) -> int:
+    """
+    Solution for part 1 of day 2,
+    We go trough every report to see if the difference between report[i+1] and report[i]
+    is of the same sign as report[i] and report[i-1]. To do so, we just have to multiply
+    both differences together, if the product is negative, it means that they are of opposite signs.
+    Then, we have to check the absolute difference between report[i+1] and report[i]. It should be
+    between 1 and 3.
+    If any of those conditions is not respected, we break from the loop over the report. Otherwise
+    we increase the count by 1.
+    **Worst-case time complexity : O(n)**
+    
+    Explanation :
+      - If we say that n is the total number of events, then it is O(n), pretty trivial since we go 
+      through every element at most two times.
+    
+    Args:
+        reports (List[List[int]]): List of all reports
+
+    Returns:
+        int: the number of safe reports
+    """
     count = 0 # The number of safe reports
     for r in reports:
         m = len(r)
